e5f34846a678970a7e722094c0eaac9663f5b8fb
Menelaos Gkotsopoulos <menelaosgkotsopoulos@gmail.com>
2025-02-24 15:28:24 +0200

    commit

--- app.js
+++ app.js
@@ -32,19 +32,69 @@
 //
 // console.log(personalData[1]);
 
-const hobbies = ["Sports", "Cooking"];
-hobbies.push("Reading"); // adds another entry at the END of the array
-hobbies.unshift("Coding"); // adds another entry at the BEGINNING of the array
-hobbies.pop(); // deletes the LAST item of the array
-hobbies.shift(); // deletes the FIRST item of the array
+// const hobbies = ["Sports", "Cooking"];
+// hobbies.push("Reading"); // adds another entry at the END of the array
+// hobbies.unshift("Coding"); // adds another entry at the BEGINNING of the array
+// hobbies.pop(); // deletes the LAST item of the array
+// hobbies.shift(); // deletes the FIRST item of the array
+//
+// console.log(hobbies);
+// hobbies[1] = "Coding"
+// hobbies[5] = "Reading" // rarely used
+// console.log(hobbies);
+//
+// hobbies.splice(1, 0, "Good Food") // looks at index 1 deletes 0 entries and adds the "Good Food"
+// console.log(hobbies);
+//
+// hobbies.splice(0, 1);
+// console.log(hobbies);
 
-console.log(hobbies);
-hobbies[1] = "Coding"
-hobbies[5] = "Reading" // rarely used
-console.log(hobbies);
+// const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
+// // const storedResults = testResults.slice(1, 5);
+// const storedResults = testResults.concat([4, 6, 7, 2]);
+// testResults.push(5.91);
+//
+// console.log(storedResults, testResults);
+// console.log(testResults.indexOf(10));
+//
+// const personData = [{name: "Menelaos"},{name: "Miguel"}];
+// console.log(personData.indexOf({name: "Menelaos"}));
+//
+// const menelaos = personData.find((person, index, persons) => {
+//     return person.name === "Menelaos";
+// });
+// console.log(menelaos);
 
-hobbies.splice(1, 0, "Good Food") // looks at index 1 deletes 0 entries and adds the "Good Food"
-console.log(hobbies);
+// const prices = [5.3, 1.5, 10.99];
+// const tax = 0.24;
+// const taxAdjustedPrices = [];
+//
+// for (const price of prices) {
+//     taxAdjustedPrices.push(price * (1 + tax));
+// }
 
-hobbies.splice(0, 1);
-console.log(hobbies);
+// prices.forEach((price, index, prices) => {
+//     const priceObj = {index: index, taxAdjPrice: price * (1 + tax)};
+//     taxAdjustedPrices.push(priceObj);
+// })
+// console.log(taxAdjustedPrices);
+
+const prices = [5.3, 1.5, 10.99];
+const tax = 0.24;
+
+const taxAdjustedPrices = prices.map((price, index, prices) => {
+    const priceObj = {index: index, taxAdjPrice: price * (1 + tax)};
+    return priceObj;
+})
+console.log(prices, taxAdjustedPrices);
+
+const sortedPrices = prices.sort((a, b) => {
+    if (a > b) {
+        return 1;
+    } else if (a === b) {
+        return 0;
+    } else {
+        return -1;
+    }
+});
+console.log("sorted prices are " + sortedPrices);
